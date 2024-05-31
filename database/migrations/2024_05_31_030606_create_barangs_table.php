fe0da11a1b0d50da1bec72c3c54e7f054f3ba3c2
agungape <muh.agungdwijuniawan@gmail.com>
2024-05-31 11:18:39 +0800

    create database

---
+++ database/migrations/2024_05_31_030606_create_barangs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('serial_number')->unique();
+            $table->string('nama');
+            $table->dateTime('tgl_beli');
+            $table->text('gambar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
---
+++ database/migrations/2024_05_31_031410_create_riwayats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('riwayats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('barang_id')->constrained()->onDelete('cascade');
+            $table->foreignId('ruangan_id')->constrained()->onDelete('cascade');
+            $table->dateTime('tgl_masuk');
+            $table->dateTime('tgl_keluar');
+            $table->string('penanggung_jawab');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('riwayats');
+    }
+};
