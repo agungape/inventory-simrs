13c22995027846d89124322e07811e048b59124b
are-one <arwanpriantomangidi@gmail.com>
2026-01-22 22:21:09 +0900

    Update icon hal. login

--- resources/views/layouts/master-login.blade.php
+++ resources/views/layouts/master-login.blade.php
@@ -10,8 +10,8 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{ asset('assets') }}/img/favicon.png" rel="icon">
-    <link href="{{ asset('assets') }}/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{ asset('assets') }}/img/icon-smc-header.png" rel="icon">
+    <link href="{{ asset('assets') }}/img/icon-smc-header.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
